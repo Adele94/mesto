7f3bc20d9b92f9db123311aebdbc9dd98fe9849c
Adel Shavalieva <adel.shavalieva@yandex.ru>
2021-06-21 16:24:43 +0300

    feat: add elements and footer

--- scripts/index.js
+++ scripts/index.js
@@ -3,27 +3,21 @@ const popupOpenButtonElement = document.querySelector('.profile__button_edit')
 const popupElement = document.querySelector('.popup')
 const popupCloseButtonElement = popupElement.querySelector('.popup__close')
 
-/**
- * Функция, которая «переключает» состояние всплывающего окошка
- */
+const openPopup = function() {
+  popupElement.classList.add('popup_is-opened')
+}
   
-  const openPopup = function() {
-    popupElement.classList.add('popup_is-opened')
-  }
-  
-  const closePopup = function() {
-    popupElement.classList.remove('popup_is-opened')
-  }
+const closePopup = function() {
+  popupElement.classList.remove('popup_is-opened')
+}
 
 const closePopupByClickOnOverlay = function(event) {
     if (event.target !== event.currentTarget) {
       return
     }
-  
     closePopup()
   }
 
-// Регистрируем обработчики событий по клику
 popupOpenButtonElement.addEventListener('click', openPopup)
 popupCloseButtonElement.addEventListener('click', closePopup)
 popupElement.addEventListener('click', closePopupByClickOnOverlay)
@@ -36,23 +30,20 @@ let profileDescription = document.querySelector('.profile__description')
 let nameInput = document.querySelector('.popup__input_name')
 let descriptionInput = document.querySelector('.popup__input_description')
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function popupSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    let nameValue = nameInput.getAttribute('value')
-    let descriptionValue = descriptionInput.getAttribute('value')
-    // Выберите элементы, куда должны быть вставлены значения полей
-    // Вставьте новые значения с помощью textContent
-    profileName.textContent = nameValue
-    descriptionInput = descriptionValue
+    evt.preventDefault();
+    let nameValue = nameInput.value
+    let descriptionValue = descriptionInput.value
+    profileName.innerHTML = `<h2 class="profile__name">${nameValue}</h2>`
+    profileDescription.innerHTML = `<p class="profile__description">${descriptionValue}</p>`
     closePopup()
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', popupSubmitHandler); 
+formElement.addEventListener('submit', popupSubmitHandler)
+
+function likeClick(event) {
+  event.preventDefault()
+  event.currentTarget.classList.toggle('element__like_active')
+}
+
+let likeBtns= document.querySelectorAll('.element__like')
