5dda299e653611989f2dd60bf5e691a06950e86d
Adel Shavalieva <adel.shavalieva@yandex.ru>
2021-06-22 18:34:35 +0300

    fix: fix bug with BEM, border and extra classes

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,17 @@ const popupOpenButtonElement = document.querySelector('.profile__button-edit')
 const popupElement = document.querySelector('.popup')
 const popupCloseButtonElement = popupElement.querySelector('.popup__close')
 
+const formElement = document.querySelector('.popup__content')
+
+let profileName = document.querySelector('.profile__name')  
+let profileDescription = document.querySelector('.profile__description')
+
+let nameInput = document.querySelector('.popup__input_bio_name')
+let descriptionInput = document.querySelector('.popup__input_bio_description')
+
 const openPopup = function() {
+  nameInput.value = profileName.textContent
+  descriptionInput.value = profileDescription.textContent
   popupElement.classList.add('popup_is-opened')
 }
   
@@ -10,39 +20,17 @@ const closePopup = function() {
   popupElement.classList.remove('popup_is-opened')
 }
 
-const closePopupByClickOnOverlay = function(event) {
-    if (event.target !== event.currentTarget) {
-      return
-    }
-    closePopup()
-  }
-
 popupOpenButtonElement.addEventListener('click', openPopup)
 popupCloseButtonElement.addEventListener('click', closePopup)
-popupElement.addEventListener('click', closePopupByClickOnOverlay)
-
-const formElement = document.querySelector('.popup__content')
-
-let profileName = document.querySelector('.profile__name')  
-let profileDescription = document.querySelector('.profile__description')
-
-let nameInput = document.querySelector('.popup__input_name')
-let descriptionInput = document.querySelector('.popup__input_description')
 
 function popupSubmitHandler (evt) {
     evt.preventDefault();
     let nameValue = nameInput.value
     let descriptionValue = descriptionInput.value
-    profileName.innerHTML = `<h2 class="profile__name">${nameValue}</h2>`
-    profileDescription.innerHTML = `<p class="profile__description">${descriptionValue}</p>`
+
+    profileName.textContent = nameValue
+    profileDescription.textContent = descriptionValue
     closePopup()
 }
 
 formElement.addEventListener('submit', popupSubmitHandler)
-
-function likeClick(event) {
-  event.preventDefault()
-  event.currentTarget.classList.toggle('element__like_active')
-}
-
-let likeBtns= document.querySelectorAll('.element__like')
